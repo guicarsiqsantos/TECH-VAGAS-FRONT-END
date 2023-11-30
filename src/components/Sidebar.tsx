0a0c5269c971d506393be85c7e8a6af44bc150a8
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-11-29 22:19:16 -0300

    Alterando Icon do sideBar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,8 @@ import { NavLink } from "react-router-dom";
 import imgLogoTechVaga from "../assets/images/logo_techvagas.png";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ApartmentIcon from "@mui/icons-material/Apartment";
 
 export default function Sidebar() {
   return (
@@ -13,61 +15,6 @@ export default function Sidebar() {
               <img src={imgLogoTechVaga} alt="tech vagas" />
             </NavLink>
           </h2>
-          {/* <div className="space-y-1">
-            <Button variant="secondary" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <circle cx="12" cy="12" r="10" />
-                <polygon points="10 8 16 12 10 16 10 8" />
-              </svg>
-              <NavLink to="/">Home</NavLink>
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <rect width="7" height="7" x="3" y="3" rx="1" />
-                <rect width="7" height="7" x="14" y="3" rx="1" />
-                <rect width="7" height="7" x="14" y="14" rx="1" />
-                <rect width="7" height="7" x="3" y="14" rx="1" />
-              </svg>
-              <NavLink to="/dashboard">Dashboard</NavLink>
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M4.9 19.1C1 15.2 1 8.8 4.9 4.9" />
-                <path d="M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5" />
-                <circle cx="12" cy="12" r="2" />
-                <path d="M16.2 7.8c2.3 2.3 2.3 6.1 0 8.5" />
-                <path d="M19.1 4.9C23 8.8 23 15.1 19.1 19" />
-              </svg>
-              <NavLink to="/dashboard/empresas">Empresas</NavLink>
-            </Button>
-          </div> */}
         </div>
         <div className="px-3 py-2 text-white">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
@@ -97,38 +44,14 @@ export default function Sidebar() {
             </NavLink>
             <NavLink to="/dashboard">
               <Button variant="ghost" className="w-full justify-start">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="mr-2 h-4 w-4"
-                >
-                  <circle cx="8" cy="18" r="4" />
-                  <path d="M12 18V2l7 4" />
-                </svg>
+                <DashboardIcon style={{ width: "15px", marginRight: "9px" }} />
                 Dashboard
               </Button>
             </NavLink>
 
             <NavLink to="/dashboard/empresas">
               <Button variant="ghost" className="w-full justify-start">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="mr-2 h-4 w-4"
-                >
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                  <circle cx="12" cy="7" r="4" />
-                </svg>
+                <ApartmentIcon style={{ width: "15px", marginRight: "9px" }} />
                 Empresas
               </Button>
             </NavLink>
