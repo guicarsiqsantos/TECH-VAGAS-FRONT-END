9b47b7b2ed984e8b0c5bbb5894e68b66d2d6686c
guicarsiqsantos <guicarsiqsantos@gmail.com>
2023-11-29 23:32:44 -0300

    Criando a dashboard do sistema

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,59 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import ApartmentIcon from "@mui/icons-material/Apartment";
+
 export default function Dashboard() {
-  return <div>dashboard</div>;
+  return (
+    <div className="flex gap-3 justify-around">
+      <Card className="w-[50%] h-36">
+        <CardHeader>
+          <div className="flex justify-between">
+            <div>
+              <CardTitle>Empresas</CardTitle>
+              <CardDescription>Empresas Cadastradas</CardDescription>
+            </div>
+            <ApartmentIcon />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <span className="text-lg">0</span>
+        </CardContent>
+      </Card>
+      <Card className="w-[50%] h-36">
+        <CardHeader>
+          <div className="flex justify-between">
+            <div>
+              <CardTitle>Alunos</CardTitle>
+              <CardDescription>Alunos Cadastradas</CardDescription>
+            </div>
+            <ApartmentIcon />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <span className="text-lg">0</span>
+        </CardContent>
+      </Card>
+      <Card className="w-[50%] h-36">
+        <CardHeader>
+          <div className="flex justify-between">
+            <div>
+              <CardTitle>Vagas</CardTitle>
+              <CardDescription>Vagas Cadastradas</CardDescription>
+            </div>
+            <ApartmentIcon />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <span className="text-lg">0</span>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
