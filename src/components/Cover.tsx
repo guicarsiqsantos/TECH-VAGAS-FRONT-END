9cac9598870584816f168297536739525615306e
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-03-07 09:04:15 -0300

    Desenvolvendo o busca de vagas rapida na HOME/cover

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -1,10 +1,10 @@
 import { Button } from "./ui/button";
 import imgMemLaptop from "../assets/images/mem_laptop.png";
-import { SearchIcon } from "lucide-react";
+import { Ghost, SearchIcon } from "lucide-react";
 
 export default function Cover() {
   return (
-    <div className="flex justify-between bg-[#00AAF2] h-[50vh]">
+    <div className="flex justify-between bg-gradient-to-t from-[#73C1E3] to-[#008DC9]  h-[50vh]">
       <div className="flex flex-col w-[52%] justify-center items-center ml-[10%] max-md:w-full max-md:ml-1">
         <h1 className="w-[70%] mb-4 text-[40px] font-bold text-white">
           Encontre sua Vaga de estágio
@@ -21,6 +21,26 @@ export default function Cover() {
               <SearchIcon className="mr-1" /> Buscar
             </Button>
           </span>
+        </div>
+        <div className=" flex w-[70%] mt-6">
+          <Button
+            variant={"outline"}
+            className="bg-[] text-white text-lg font-normal rounded-sm mr-3"
+          >
+            Vagas para Tecnologia
+          </Button>
+          <Button
+            variant={"outline"}
+            className="bg-[] text-white text-lg font-normal rounded-sm  mr-3"
+          >
+            Vagas para Gestão Empresarial
+          </Button>
+          <Button
+            variant={"outline"}
+            className="bg-[] text-white text-lg font-normal rounded-sm  mr-3"
+          >
+            Mais
+          </Button>
         </div>
       </div>
       <div className="mr-[10%] max-md:hidden">
