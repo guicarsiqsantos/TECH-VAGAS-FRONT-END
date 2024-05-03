11a029c8f5806a60ffcfae1f68593604a97f963c
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-05-03 09:18:21 -0300

    Começando a desenvolver as listagem das vagas

--- src/components/vagas.tsx
+++ src/components/vagas.tsx
@@ -1,8 +1,25 @@
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import ApartmentIcon from "@mui/icons-material/Apartment";
+import { VagasProps } from "@/pages/dashboard/Vagas/table/columns";
+import { useEffect, useState } from "react";
+import api from "@/services/api";
 
 const Vagas = () => {
+  const [data, setData] = useState<VagasProps[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const data: VagasProps[] = await (await api.get("/vagas")).data;
+
+      const includeKeyData = data.map((item, idx) => {
+        return { ...item, key: idx };
+      });
+
+      setData(includeKeyData);
+    })();
+  }, []);
+
   return (
     <div className="flex mb-16">
       <div className="mr-4">
@@ -11,6 +28,7 @@ const Vagas = () => {
       <div>
         <h1 className="text-[#22C964] font-medium">
           Programa de Estágio Vallourec 2023 - Nível Superior
+          {/* {data.map((item) => item.descricao)} */}
         </h1>
         <p className="pb-3">Estágio - 20 vagas</p>
         <p>
