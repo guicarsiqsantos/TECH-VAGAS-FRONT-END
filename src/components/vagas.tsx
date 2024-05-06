34c99cad17e7644ea0330543284cb5f933c3cb86
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-05-06 10:15:48 -0300

    Puxando as vagas do banco de dados e exibindo para o aluno

--- src/components/vagas.tsx
+++ src/components/vagas.tsx
@@ -1,54 +1,58 @@
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import ApartmentIcon from "@mui/icons-material/Apartment";
-import { VagasProps } from "@/pages/dashboard/Vagas/table/columns";
 import { useEffect, useState } from "react";
 import api from "@/services/api";
+import ApartmentIcon from "@mui/icons-material/Apartment";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import { VagasProps } from "@/pages/dashboard/Vagas/table/columns";
 
 const Vagas = () => {
   const [data, setData] = useState<VagasProps[]>([]);
 
   useEffect(() => {
-    (async () => {
-      const data: VagasProps[] = await (await api.get("/vagas")).data;
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/vagas");
+        const fetchedData: VagasProps[] = response.data;
 
-      const includeKeyData = data.map((item, idx) => {
-        return { ...item, key: idx };
-      });
+        // Add 'key' property to each item for rendering purposes
+        const dataWithKeys = fetchedData.map((item, idx) => ({
+          ...item,
+          key: idx,
+        }));
 
-      setData(includeKeyData);
-    })();
+        setData(dataWithKeys);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div className="flex mb-16">
-      <div className="mr-4">
-        <ApartmentIcon />
-      </div>
-      <div>
-        <h1 className="text-[#22C964] font-medium">
-          Programa de Estágio Vallourec 2023 - Nível Superior
-          {/* {data.map((item) => item.descricao)} */}
-        </h1>
-        <p className="pb-3">Estágio - 20 vagas</p>
-        <p>
-          Descrição: Como será seu dia a dia:Você desenvolverá ajustes, novas
-          funcionalidades e atuará na resolução de débitos técnicos nos nossos
-          produtos para profissionais e empresas;Você utilizará Ruby on Rails,
-          RabbitMQ, SQL Server, Elasticsearch e AWS;Trabalhará na arquitetura e
-          modernização de aplicações, permitindo ainda mais agilidade,...
-        </p>
-        <div className="flex justify-between mt-4">
-          <div className="flex">
-            <LocationOnIcon className="mr-2" />
-            <p>Rio das Ostras / RJ</p>
+    <div>
+      {data.map((vaga) => (
+        <div key={vaga.vagasId} className="flex mb-16">
+          <div className="mr-4">
+            <ApartmentIcon />
           </div>
-          <div className="flex">
-            <AccessTimeIcon />
-            <p>27/07/2023</p>
+          <div className="w-full">
+            <h1 className="text-[#22C964] font-medium">{vaga.titulo}</h1>
+            <p className="pb-3">{`Estágio - ${vaga.quantidade} vagas`}</p>
+            <p>{vaga.descricao}</p>
+            <div className="flex justify-between mt-4">
+              <div className="flex">
+                <LocationOnIcon className="mr-2" />
+                <p>{vaga.localidade}</p>
+              </div>
+              <div className="flex">
+                <AccessTimeIcon />
+                <p>{vaga.dataLimite}</p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
