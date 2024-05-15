bcd11ae83e84be10765aedb24b3b5083a2004fac
Victor Hugo <vhfn2004@gmail.com>
2024-05-15 16:42:01 -0300

    feito a colum de cargo

---
+++ src/pages/dashboard/cargo/table/columns.tsx
@@ -0,0 +1,93 @@
+import { Button } from "@/components/ui/button";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { toast } from "sonner";
+import api from "@/services/api";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Link } from "react-router-dom";
+
+export type CargoProps = {
+    CargoId: number;
+    Descricao: string;
+    Tipo: string;
+    key: number; 
+};
+
+export const columns: ColumnDef<CargoProps>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value: any) =>
+          table.toggleAllPageRowsSelected(!!value)
+        }
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value: any) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "Descricao",
+    header: "Descrição",
+  },
+  {
+    accessorKey: "Tipo",
+    header: "Tipo",
+  },
+  {
+    id: "actions",
+    header: "Ação",
+    cell: ({ row, table }) => {
+      const meta = table.options.meta;
+      const dataRow = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <Link to={`/dashboard/cargo/cadastro/${dataRow.CargoId}`}>
+              <DropdownMenuItem>📝 Editar</DropdownMenuItem>
+            </Link>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={async () => {
+                toast("Empresa Exluido com Sucesso. ✅");
+                meta?.removeRow(dataRow.key);
+                await api.delete(`/concedente/${dataRow.CargoId}`);
+              }}
+            >
+              🗑️ delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
