07fe077d8f2a1fdd5008cefb9184f89be684104c
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-05-23 10:10:01 -0300

    Adicionando o dialog e correçao do sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,11 +12,13 @@ export default function Sidebar() {
     <div className={cn("pb-12 w-[250px] shadow bg-[#1a1a1a]")}>
       <div className="flex-1 space-y-4 py-4">
         <div className="px-3 py-2 ">
-          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tigh">
-            <NavLink to="/">
-              <img src={imgLogoTechVaga} alt="tech vagas" />
-            </NavLink>
-          </h2>
+          <NavLink to="/">
+            <img
+              className="mb-2 px-4 text-lg font-semibold tracking-tigh"
+              src={imgLogoTechVaga}
+              alt="tech vagas"
+            />
+          </NavLink>
         </div>
         <div className="px-3 py-2 text-white">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
@@ -75,39 +77,6 @@ export default function Sidebar() {
             </NavLink>
           </div>
         </div>
-
-        {/* <div className="py-2 text-white">
-          <h2 className="relative px-7 text-lg font-semibold tracking-tight">
-            Playlists
-          </h2>
-          <ScrollArea className="h-[300px] px-1">
-            <div className="space-y-1 p-2">
-              <Button
-                variant="ghost"
-                className="w-full justify-start font-normal"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="mr-2 h-4 w-4"
-                >
-                  <path d="M21 15V6" />
-                  <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                  <path d="M12 12H3" />
-                  <path d="M16 6H3" />
-                  <path d="M12 18H3" />
-                </svg>
-                TESTE
-              </Button>
-              
-            </div>
-          </ScrollArea>
-        </div> */}
       </div>
     </div>
   );
