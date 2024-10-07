c3cb9adbf2ac483be1b09549ad42da70d9e324e0
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-10-07 16:18:31 -0300

    pasando o token no header do backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,19 @@ const api = axios.create({
   baseURL: "https://localhost:7188/api/",
 });
 
+// Interceptor para adicionar o header antes de cada requisição
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    config.headers['Content-Type'] = 'application/json';
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
